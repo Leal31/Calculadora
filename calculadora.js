8d7c145b1bf4f9217f166fe219d19d2ff2530710
Emanuel Leal Arce <63663526+Leal31@users.noreply.github.com>
2020-08-22 18:38:07 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+var barra=document.getElementById("barra);
+var cero=document.getElementById("0");
+var uno=document.getElementById("1");
+var dos=document.getElementById("2");
+var tres=document.getElementById
