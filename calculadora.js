ace614707f795ad410835e41ea66bfe8d6de0687
Leal31 <lealemanuel31@gmail.com>
2020-08-23 11:00:08 -0500

    Añadir todos los bloques a javascript

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
-var barra=document.getElementById("barra);
+var barra=document.getElementById("barra");
 var cero=document.getElementById("0");
 var uno=document.getElementById("1");
 var dos=document.getElementById("2");
-var tres=document.getElementById
+var tres=document.getElementById("3");
+var cuatro=document.getElementById("4");
+var cinco=document.getElementById("5");
+var seis=document.getElementById("6");
+var siete=document.getElementById("7");
+var ocho=document.getElementById("8");
+var nueve=document.getElementById("9");
